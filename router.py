871c9959deee81d16e5dba6a623c2d65bb575075
ijsbol <izzy@uwu.gal>
2024-08-23 15:57:52 +0100

    feat: CORS Handling

--- router.py
+++ router.py
@@ -1,19 +1,19 @@
 from os import getenv
 from typing import Final, cast
 from fastapi import FastAPI
-from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.api.spotify import router as get_spotify_current_song_router
-
-
-SESSION_MIDDLEWARE_SECRET_KEY: Final[str] = cast(str, getenv("SESSION_MIDDLEWARE_SECRET_KEY"))
 
 
 app = FastAPI()
 
 app.add_middleware(
-    middleware_class=SessionMiddleware,
-    secret_key=SESSION_MIDDLEWARE_SECRET_KEY,
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(get_spotify_current_song_router, prefix="")
